9f4c28bbee1a5dbe3df772ca3f81da7a956d462b
Forest Defenders Club NFTs <bvkaura@gmail.com>
2023-11-07 23:45:00 +0530

    implemented search and ViewModel in Settings View

--- swiftuilearning/settingsView.swift
+++ swiftuilearning/settingsView.swift
@@ -13,9 +13,31 @@ struct tileModel : Identifiable {
     var icon : String
     var name : String
 }
+
+class searchViewModel : ObservableObject {
+    let settingsList : [tileModel] = [
+        tileModel(icon:"airplane", name: "Aeroplane Mode") ,
+        tileModel(icon:"wifi", name: "Wi-Fi"),
+        tileModel(icon:"antenna.radiowaves.left.and.right.circle.fill", name: "Bluetooth"),
+        
+        tileModel(icon:"antenna.radiowaves.left.and.right.circle", name: "Cellular"),
+        tileModel(icon:"bell.badge.fill", name: "Notifications") ,
+        tileModel(icon:"speaker.wave.3.fill", name: "Sounds & Haptics"),
+        tileModel(icon:"moon.fill", name: "Focus"),
+        tileModel(icon:"iphone.gen2", name: "Screen Time"),
+        
+        tileModel(icon:"gear", name: "General") ,
+        tileModel(icon:"switch.2", name: "Control Centre"),
+        tileModel(icon:"alarm.fill", name: "Alarm"),
+        tileModel(icon:"iphone.smartbatterycase.gen2",
+                  name: "Display & Brightness"),
+        
+    ]
+    @Published var searchQuery : String = ""
+}
 struct settingsView: View {
-    
-    @State var searchQuery : String = ""
+
+    @ObservedObject var viewModel = searchViewModel()
     var body: some View {
         NavigationView {
             
@@ -32,7 +54,7 @@ struct settingsView: View {
                                 .frame(width: 18, height: 18)
                             
                             Spacer().frame(width: 8)
-                            TextField(text: $searchQuery) {
+                            TextField(text: $viewModel.searchQuery) {
                                 Text("Search...")
                             }
                             
@@ -46,118 +68,140 @@ struct settingsView: View {
                         }.padding(12).background(Color(.systemBackground)).cornerRadius(10)
                         
                         Spacer().frame(height: 20)
-                        VStack {
-                            HStack {
-                                Image("story1").resizable()
-                                    .aspectRatio(contentMode: .fill)
-                                .frame(width: 52, height: 52)
-                                .cornerRadius(90)
-                                
-                                Spacer().frame(width: 16)
-                                VStack {
-                                    HStack {
-                                        VStack {
-                                            HStack {
-                                                Text("Bhaskar Kaura").font(.system(size: 20, weight: .medium))
-                                                    .frame(alignment: .leading)
-                                                    .multilineTextAlignment(.leading)
-                                                
-                                                Spacer()
-                                            }
-                                            Spacer().frame(height: 4)
-                                            HStack {
-                                                Text("Apple Id, ICloud+, Media & Purchases").font(.system(size: 13, weight: .regular))
-                                                Spacer()
-                                            }
-                                            Spacer()
-                                            
-                                        }.padding(.top, 6)
-                                        
-                                        Spacer()
-                                        Image(systemName: "chevron.right").resizable().aspectRatio(contentMode: .fit)
-                                            .foregroundColor(Color(.systemGray2))
-                                            .frame(width: 14, height: 14)
-                                    }
-                                    
-                                    Divider()
-                                        .padding(.vertical,6)
-                                    
+                        
+                        if(!viewModel.searchQuery.isEmpty && viewModel.searchQuery != "") {
+                            VStack {
+                                ForEach(searchResults) { result in
+                                    searchView(tileList: [result]
+                                                ,title: "Search results")
                                 }
-                             
                             }
-                            
-                            HStack {
+                        
+                        }
+                        else {
+                            VStack {
                                 HStack {
-                                    Image("person2").resizable()
+                                    Image("story1").resizable()
                                         .aspectRatio(contentMode: .fill)
-                                        .frame(width: 32, height: 32)
+                                        .frame(width: 52, height: 52)
                                         .cornerRadius(90)
                                     
-                                    Image("person1").resizable()
-                                        .aspectRatio(contentMode: .fill)
-                                        .frame(width: 32, height: 32)
-                                        .cornerRadius(90)
-                                        .padding(.leading, -20)
-                                }
-                                
-                                Spacer().frame(width: 16)
-                            
-                                        VStack {
-                                            HStack {
-                                                Text("Family").font(.system(size: 16, weight: .medium))
-                                                    .frame(alignment: .leading)
-                                                    .multilineTextAlignment(.leading)
-                                                
+                                    Spacer().frame(width: 16)
+                                    VStack {
+                                        HStack {
+                                            VStack {
+                                                HStack {
+                                                    Text("Bhaskar Kaura").font(.system(size: 20, weight: .medium))
+                                                        .frame(alignment: .leading)
+                                                        .multilineTextAlignment(.leading)
+                                                    
+                                                    Spacer()
+                                                }
+                                                Spacer().frame(height: 4)
+                                                HStack {
+                                                    Text("Apple Id, ICloud+, Media & Purchases").font(.system(size: 13, weight: .regular))
+                                                    Spacer()
+                                                }
                                                 Spacer()
-                                            }
+                                                
+                                            }.padding(.top, 6)
+                                            
+                                            Spacer()
+                                            Image(systemName: "chevron.right").resizable().aspectRatio(contentMode: .fit)
+                                                .foregroundColor(Color(.systemGray2))
+                                                .frame(width: 14, height: 14)
                                         }
                                         
-                                        Spacer()
-                                        Image(systemName: "chevron.right").resizable().aspectRatio(contentMode: .fit)
-                                            .foregroundColor(Color(.systemGray2))
-                                            .frame(width: 14, height: 14)
-                                  
-                             
-                            }
-                        
-                        }.padding(.vertical, 8).padding(.horizontal, 14).background(Color(.systemBackground)).cornerRadius(10)
-                        
-                        Spacer().frame(height: 20)
-                        sectionView(tileList: [
-                            tileModel(icon:"airplane", name: "Aeroplane Mode") ,
-                            tileModel(icon:"wifi", name: "Wi-Fi"),
-                            tileModel(icon:"antenna.radiowaves.left.and.right.circle.fill", name: "Bluetooth"),
-                            tileModel(icon:"antenna.radiowaves.left.and.right.circle", name: "Cellular"),
+                                        Divider()
+                                            .padding(.vertical,6)
+                                        
+                                    }
+                                    
+                                }
+                                
+                                HStack {
+                                    HStack {
+                                        Image("person2").resizable()
+                                            .aspectRatio(contentMode: .fill)
+                                            .frame(width: 32, height: 32)
+                                            .cornerRadius(90)
+                                        
+                                        Image("person1").resizable()
+                                            .aspectRatio(contentMode: .fill)
+                                            .frame(width: 32, height: 32)
+                                            .cornerRadius(90)
+                                            .padding(.leading, -20)
+                                    }
+                                    
+                                    Spacer().frame(width: 16)
+                                    
+                                    VStack {
+                                        HStack {
+                                            Text("Family").font(.system(size: 16, weight: .medium))
+                                                .frame(alignment: .leading)
+                                                .multilineTextAlignment(.leading)
+                                            
+                                            Spacer()
+                                        }
+                                    }
+                                    
+                                    Spacer()
+                                    Image(systemName: "chevron.right").resizable().aspectRatio(contentMode: .fit)
+                                        .foregroundColor(Color(.systemGray2))
+                                        .frame(width: 14, height: 14)
+                                    
+                                    
+                                }
+                                
+                            }.padding(.vertical, 8).padding(.horizontal, 14).background(Color(.systemBackground)).cornerRadius(10)
                             
-                        ],title: "Network & Connectivity")
-                        
-                        Spacer().frame(height: 20)
-                        
-                        sectionView(tileList: [
-                            tileModel(icon:"bell.badge.fill", name: "Notifications") ,
-                            tileModel(icon:"speaker.wave.3.fill", name: "Sounds & Haptics"),
-                            tileModel(icon:"moon.fill", name: "Focus"),
-                            tileModel(icon:"iphone.gen2", name: "Screen Time"),
+                            Spacer().frame(height: 20)
+                            sectionView(tileList: [
+                                tileModel(icon:"airplane", name: "Aeroplane Mode") ,
+                                tileModel(icon:"wifi", name: "Wi-Fi"),
+                                tileModel(icon:"antenna.radiowaves.left.and.right.circle.fill", name: "Bluetooth"),
+                                tileModel(icon:"antenna.radiowaves.left.and.right.circle", name: "Cellular"),
+                                
+                            ],title: "Network & Connectivity")
                             
-                        ],title: "System")
-                        
-                        Spacer().frame(height: 20)
-                        
-                        sectionView(tileList: [
-                            tileModel(icon:"gear", name: "General") ,
-                            tileModel(icon:"switch.2", name: "Control Centre"),
-                            tileModel(icon:"alarm.fill", name: "Alarm"),
-                            tileModel(icon:"iphone.smartbatterycase.gen2", name: "Display & Brightness"),
+                            Spacer().frame(height: 20)
                             
-                        ],title: "General")
+                            sectionView(tileList: [
+                                tileModel(icon:"bell.badge.fill", name: "Notifications") ,
+                                tileModel(icon:"speaker.wave.3.fill", name: "Sounds & Haptics"),
+                                tileModel(icon:"moon.fill", name: "Focus"),
+                                tileModel(icon:"iphone.gen2", name: "Screen Time"),
+                                
+                            ],title: "System")
+                            
+                            Spacer().frame(height: 20)
+                            
+                            sectionView(tileList: [
+                                tileModel(icon:"gear", name: "General") ,
+                                tileModel(icon:"switch.2", name: "Control Centre"),
+                                tileModel(icon:"alarm.fill", name: "Alarm"),
+                                tileModel(icon:"iphone.smartbatterycase.gen2", name: "Display & Brightness"),
+                                
+                            ],title: "General")
+                            
+                        }
                         
-                        Spacer()
-                    }.padding(.horizontal)
-                })
+                            Spacer()
+                        }.padding(.horizontal)
+                    })
                 
             }  .navigationTitle("Settings")
             
         }
+    }
+    
+    var searchResults: [tileModel] {
+        if viewModel.searchQuery.isEmpty {
+            return viewModel.settingsList
+         } else {
+             return viewModel.settingsList.filter { $0.name.lowercased().contains(viewModel.searchQuery.lowercased()) }
+         }
+     
     }
 }
 
@@ -210,6 +254,19 @@ struct sectionView : View {
     }
 }
 
+struct searchView : View {
+    var tileList : [tileModel] = []
+    var title : String
+    var body : some View {
+        Section {
+            ForEach(tileList) { list in
+                settingsTileView(icon: list.icon, name: list.name)
+            }
+          
+        }
+    }
+}
+
 struct ExDivider: View {
     let color: Color = .gray
     let width: CGFloat = 1
@@ -221,17 +278,7 @@ struct ExDivider: View {
     }
 }
 
-struct customerRoundedCorners: View {
-    let radius = 12 // radius we need
-    var body: some View {
-        Rectangle()
-            .frame(height: 50)
-            .foregroundColor(.black)
-        .padding(.bottom, 0)
-        .cornerRadius(10)
-        .padding(.bottom, -10)
-    }
-}
+
 #Preview {
     settingsView()
 }
